e9f6c814ffab205e87671a281f85f6bce24699d5
Pablo Messina <pamessina@uc.cl>
2020-08-13 21:31:58 -0400

    update

--- docs/_includes/template.cpp
+++ docs/_includes/template.cpp
@@ -1,23 +1,20 @@
-#pragma GCC optimize("Ofast") // OPTIONAL: for faster execution
-#include <bits/stdc++.h> // import everything in one shot
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
 using namespace std;
 // defines
 #define rep(i,a,b) for(int i = a; i < b; ++i)
 #define invrep(i,b,a) for(int i = b; i >= a; --i)
 #define umap unordered_map
 #define uset unordered_set
+#define ff first
+#define ss second
 // typedefs
-typedef unsigned int uint;
+typedef pair<int,int> ii;
 typedef unsigned long long int ull;
 typedef long long int ll;
-typedef vector<int> vi;
-typedef pair<int,int> ii;
-typedef tuple<int,int,int> iii;
 // -------------------------------
-
 int main() {
-    // OPTIONAL: for faster input/output (only if using cin/cout)
     ios::sync_with_stdio(false); 
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
     return 0;
 }
